a37c7b7eb331bde52d7cfbbff4bd48bedf63e435
Randy <rcq1@students.uwf.edu>
2019-10-01 21:23:54 -0500

    added logging for test

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 let express = require('express');
 let app = express();
+const fs = require('fs');
 
 app.get('/', function(req, res) {
         res.send("this is the home page");
@@ -9,6 +10,30 @@ app.get('/', function(req, res) {
 app.get('/version', function(req, res){
         res.type('text/plain');
         res.send('This is version 0 of the HotBurger service');
+});
+
+app.get('/logs', function(req, res) {
+        res.type('text/plain');
+        fs.writeFile('./logs/logfile.txt', "hey hey hey", (err) => {
+                if (err) { 
+                        throw err; 
+                }
+                
+                console.log('text written to file!');
+        });
+
+        fs.readFile("./logs/logfile.txt", function(err, buf) {
+                res.send(buf.toString());
+                if (err) { 
+                        console.log(err); 
+                }
+        });
+});
+
+app.use(function(err, req, res, next) {
+        console.error(err.message);
+        res.type('text/plain');
+        res.status(500).send("An error has occurred");
 });
 
 app.listen(80, () => {
