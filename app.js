f885999398ab9feb8e1f64bde47af59b5cebde01
Randy <rcq1@students.uwf.edu>
2019-10-04 11:34:05 -0500

    Adjusted error handling for file read/write

--- app.js
+++ app.js
@@ -6,51 +6,49 @@ app.get('/', function(req, res) {
         res.send("this is the home page\n");
 });
 
-app.get('/version', function (req, res) {
+app.get('/version', async function (req, res, next) {
         if (res) {
-                logFunctionCall('/version');
+                logFunctionCall('/version', next);
         } else {
-                resultFailed('/version');
+                resultFailed('/version', next);
         }
 
         res.type('text/plain');
         res.send('This is version 0 of the HotBurger service\n');
 });
 
-app.get('/logs', function(req, res) {
+app.get('/logs', function(req, res, next) {
         res.type('text/plain');
         // If a result was successfully sent
         if (res) {
-               logFunctionCall('/logs');
+               logFunctionCall('/logs', next);
         } else {
-               resultFailed('/logs');
+               resultFailed('/logs', next);
         }
 
         fs.readFile("./logs/logfile.txt", function(err, buf) {
-                res.send(buf.toString());
+                if (err) {
+                        res.send("Failed to read file when calling the /logs route");
+                } else {
+                        res.send(buf.toString());
+                }                
         });
 });
 
-function logFunctionCall(route) {
+function logFunctionCall(route, next) {
         fs.appendFile('./logs/logfile.txt', route + " route was called.  The result was succesfully sent.\n", "utf8", (err) => {
                 if (err) {
-                        throw new Error('Failure to append to file in ' + route + ' route \n');
+                        next(err.message);
                 }
         });
 }
 
-function resultFailed(route) {
+function resultFailed(route, next) {
         fs.appendFile('./logs/logfile.txt', route + " route was called.  The result FAILED to send.\n", "utf8", (err) => {
                 if (err) {
-                        throw new Error('Failure to append to file in ' + route + ' route \n');
+                        next(err.message);
                 }
         });
 }
-
-app.use(function(err, req, res, next) {
-        fs.appendFile('./logs/logfile.txt', err.message, "utf8");
-        res.type('text/plain');
-        res.status(500).send("An error has occurred.");
-});
 
 module.exports = app;
