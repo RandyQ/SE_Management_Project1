adaebce021284681f585e1ae00d8d9896798a67d
Randy <rcq1@students.uwf.edu>
2019-10-03 19:52:20 -0500

    updated logger for cleaner code and added repository to package.json file

--- server.js
+++ server.js
@@ -9,17 +9,9 @@ app.get('/', function(req, res) {
 
 app.get('/version', function (req, res) {
         if (res) {
-                fs.appendFile('./logs/logfile.txt', "/version route was called.  The result was succesfully sent.\n", "utf8", (err) => {
-                        if (err) {
-                                throw new Err('Failure to append to file in /version route \n');
-                        }
-                });
+                logFunctionCall('/version');
         } else {
-                fs.appendFile('./logs/logfile.txt', "/version route was called.  The result FAILED to send.\n", "utf8", (err) => {
-                        if (err) {
-                                throw new Err('Failure to append to file in /version route \n');
-                        }
-                });
+                resultFailed('/version');
         }
 
         res.type('text/plain');
@@ -30,23 +22,31 @@ app.get('/logs', function(req, res) {
         res.type('text/plain');
         // If a result was successfully sent
         if (res) {
-                fs.appendFile('./logs/logfile.txt', "/logs route was called.  The result was succesfully sent.\n", "utf8", (err) => {
-                        if (err) {
-                                throw new Err('Failure to append to file in /logs route \n');
-                        }
-                });
+               logFunctionCall('/logs');
         } else {
-                fs.appendFile('./logs/logfile.txt', "/logs route was called.  The result FAILED to send.\n", "utf8", (err) => {
-                        if (err) {
-                                throw new Err('Failure to append to file in /logs route \n');
-                        }
-                });
+               resultFailed('/logs');
         }
 
         fs.readFile("./logs/logfile.txt", function(err, buf) {
                 res.send(buf.toString());
         });
 });
+
+function logFunctionCall(route) {
+        fs.appendFile('./logs/logfile.txt', route + " route was called.  The result was succesfully sent.\n", "utf8", (err) => {
+                if (err) {
+                        throw new Err('Failure to append to file in ' + route + ' route \n');
+                }
+        });
+}
+
+function resultFailed(route) {
+        fs.appendFile('./logs/logfile.txt', route + " route was called.  The result FAILED to send.\n", "utf8", (err) => {
+                if (err) {
+                        throw new Err('Failure to append to file in ' + route + ' route \n');
+                }
+        });
+}
 
 app.use(function(err, req, res, next) {
         fs.appendFile('./logs/logfile.txt', err.message, "utf8");
