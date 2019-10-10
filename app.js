2ab58754aed83d4532393cd2c2daacf87d2f46ad
Randy <rcq1@students.uwf.edu>
2019-10-10 17:59:05 -0500

    edited code and added promise

--- app.js
+++ app.js
@@ -3,50 +3,46 @@ let app = express();
 const fs = require('fs');
 
 app.get('/', function(req, res) {
-        res.send("this is the home page\n");
+        res.send("This is the home page\n");
 });
 
-app.get('/version', async function (req, res, next) {
-        if (res) {
-                logFunctionCall('/version', next);
-        } else {
-                resultFailed('/version', next);
-        }
-
-        res.type('text/plain');
+app.get('/version', function (req, res) {
+        // If a result was successfully sent
+        res ? logFunctionCall('/version') : resultFailed('/version');
         res.send('This is version 0 of the HotBurger service\n');
 });
 
-app.get('/logs', function(req, res, next) {
-        res.type('text/plain');
-        // If a result was successfully sent
-        if (res) {
-               logFunctionCall('/logs', next);
-        } else {
-               resultFailed('/logs', next);
-        }
-
+app.get('/logs', async function(req, res) {
+        // If a result was successfully sent, write to the log (await) before reading from it
+        res ? await logFunctionCall('/logs') : resultFailed('/logs');
         fs.readFile("./logs/logfile.txt", function(err, buf) {
                 if (err) {
-                        res.send("Failed to read file when calling the /logs route");
+                        res.send("Failed to read file when calling the /logs route")
                 } else {
-                        res.send(buf.toString());
-                }                
+                        let formattedString = "";
+                        // Add a breakpoint between each line to render the html on separate lines for each entry
+                        buf.toString().split('\n').forEach(item => { formattedString += item + "<br>" });
+                        res.send(formattedString);
+                }             
         });
 });
 
-function logFunctionCall(route, next) {
-        fs.appendFile('./logs/logfile.txt', route + " route was called.  The result was succesfully sent.\n", "utf8", (err) => {
-                if (err) {
-                        next(err.message);
-                }
+function logFunctionCall(route) {
+        return new Promise((resolve) => {
+                fs.appendFile('./logs/logfile.txt', `${route} route was called.  The result was succesfully sent.\n`, "utf8", (err) => {
+                        if (err) {
+                                console.log(`ERROR: Log failed to write to file when calling the ${route} route`);
+                        }
+
+                        resolve();
+                });
         });
 }
 
-function resultFailed(route, next) {
-        fs.appendFile('./logs/logfile.txt', route + " route was called.  The result FAILED to send.\n", "utf8", (err) => {
+function resultFailed(route) {
+        fs.appendFile('./logs/logfile.txt', `${route} route was called.  The result FAILED to send.\n`, "utf8", (err) => {
                 if (err) {
-                        next(err.message);
+                        console.log(`ERROR: Log failed to write to file when calling the ${route} route`);
                 }
         });
 }
